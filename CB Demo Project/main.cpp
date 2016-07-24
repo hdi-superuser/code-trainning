6f86b743961e849fd90e643cc8868f3f2bbef3d7
hdi-superuser <huynhduy.idol@gmail.com>
2016-07-25 02:50:59 +0700

    add demo codeblocks project

---
+++ CB Demo Project/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+using namespace std;
+
+#define task "main"
+#define N 1000001
+#define MOD 1000000007
+#define inf 0x7fffffff
+
+#define fst first
+#define snd second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define sz(x) int(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define remain(x) if (x > MOD) x -= MOD
+#define popcnt(x) __builtin_popcount(x)
+#define rep(i, j, k) for (register int i = j; i <= k; i++)
+#define dow(i, j, k) for (register int i = j; i >= k; i--)
+#define debug(x) cerr << __LINE__ << ": " << #x << " = " << (x) << endl
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+template<typename T> void in(T& x) { cin >> x; }
+template<typename T> void outln(T& x) { cout << x << endl; }
+template<typename T> void out(T& x) { cout << x; }
+template<typename T, typename X>
+    void out(T& x, X& y) { cout << x; cout << y; }
+
+struct _ { _() { ios_base::sync_with_stdio(0); } } _;
+/*----------------------- header ------------------------*/
+
+main(void) {
+    #ifndef ONLINE_JUDGE
+        freopen(task".inp", "r", stdin);
+        freopen(task".out", "w", stdout);
+    #endif // ONLINE_JUDGE
+    /*
+        Do something
+    */
+    return 0;
+}
